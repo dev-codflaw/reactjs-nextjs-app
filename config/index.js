1a8caef4d1183a556065925d822db3f067a81226
Umesh <dev.codflaw@gmail.com>
2021-03-25 07:20:43 +0530

    remote fetch url fix

--- config/index.js
+++ config/index.js
@@ -1,4 +1,5 @@
-const dev = process.env.NODE_ENV !== 'production'
+// const dev = process.env.NODE_ENV !== 'production'
+
+const dev = false
 
 export const server = dev ? 'http://localhost:8000' : 'https://cms.codflaw.com'
-// export const server = dev ? 'https://cms.codflaw.com' : 'https://cms.codflaw.com'
